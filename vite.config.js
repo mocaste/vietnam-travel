a8323426a1af34a2bce6a93e4a15439f507642d1
mocaste <stefan.mocanu@metaminds.com>
2025-07-26 23:12:06 +0300

    updated base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/vietnam-travel-site/',  // Important for GitHub Pages
+  build: {
+    outDir: 'dist'
+  }
 })
